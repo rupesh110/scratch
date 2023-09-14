6d7d0f476a1cf2b6889b164afec4afb9726e131e
Rupesh <shrestharupesh110@gmail.com>
2023-09-14 13:44:05 +1000

    added vite_target

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv';
+dotenv.config();
 
+
+
+console.log("Proxy target:", process.env.TARGET);
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    'process.env.TARGET': JSON.stringify(process.env.TARGET),
+  },
   plugins: [react()],
   server:{
     port: 3000,
     proxy: {
       '/api':{
-        target: 'http://localhost:5000',
+        target: process.env.TARGET,
         changeOrigin: true,
       }
     }
